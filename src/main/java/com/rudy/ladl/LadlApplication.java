d2e238dc571ca04a384bffa513507699ae474058
Valaragen <rudy.boinnard@gmail.com>
2019-12-14 14:08:20 +0100

    tomcat configuration

--- src/main/java/com/rudy/ladl/LadlApplication.java
+++ src/main/java/com/rudy/ladl/LadlApplication.java
@@ -3,10 +3,11 @@ package com.rudy.ladl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class LadlApplication {
+public class LadlApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LadlApplication.class, args);
